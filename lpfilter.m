7fcfd6259e26cff7c6c1cb1a57f69f2d6edad0b8
Jeremy Mattfeld <jmmattfeld@gmail.com>
2019-01-06 22:36:44 -0800

    gain testing

--- lpfilter.m
+++ lpfilter.m
@@ -6,58 +6,50 @@ clear all; close all;
 pkg load signal;
 
 % declare some path variables
-wkDir = "C:\\Users\\Jeremy.SV\\Documents\\octave-projects\\";
-inpDir = [wkDir "data\\"];
-outpDir = [wkDir "outputs\\"];
+wkDir = "/home/jeremymelinda/Source/Octave/octave-projects/";
+inpDir = [wkDir "data/"];
+outpDir = [wkDir "outputs/"];
 
 % pos step data
-data_file = [inpDir "SN008_data_pStep_12-10.csv"];
-temps = dlmread(data_file,',',"D2..K3221");
+data_file = [inpDir "All_10_50_15_1HzTmpCor.csv"];
+press = dlmread(data_file,',',"Q2..R15063");
 
-% setup time array
-frame = [1:3220];
-ftime = frame*2;
-time = ftime./60;
+% setup x array
+frame = [1:15062];
 
 % calculate cutoff freq
 fsam = 0.5;
 fnyq = fsam/2;
 flp = fnyq/16;
 
-% butterworth low pass filter
-[b,a] = butter(2, flp/fnyq);
-
-% apply butterworth filter with offset
-fTemps = filter(b,a,temps(:,1));
-
 % FIR1
-n = 9; % for a filter length of 10
+n = 23; % for a filter length of 10
 b = fir1(n,flp);
-fir1Temps = filter(b,1,temps(:,1));
+fCorPress = filter(b,1,press(:,2));
 
 figure(1); hold on;
-plot(time,temps(:,1));
-%plot(time+1,fTemps(:,1));
-plot(time,fir1Temps(:,1));
-title("comparison of digital signal filters");
-legend("unfiltered signal","fir1 (octave)",'location','southeast');
+plot(frame,press(:,1));
+plot(frame,press(:,2));
+plot(frame,fCorPress);
+title("filtered corrected pressure");
+legend("uncorrected pressure","unfiltered corrected pressure","filtered corrected pressure",'location','southeast');
 grid on;
 hold off;
 
-% export unfiltered signal for C implementation
-unfiltered = temps(:,1);
-outfile = [outpDir "unfiltered.csv"];
-csvwrite(outfile, unfiltered);
-
-% read FIR filtered output from c program
-infile = [inpDir "filtered.csv"];
-filtered = csvread(infile);
-
-figure(2); hold on;
-plot(time, unfiltered);
-plot(time, filtered);
-title("FIR filter response (C implementation)");
-legend("unfiltered signal","firC",'location','southeast');
-grid on;
-hold off;
-
+%% export unfiltered signal for C implementation
+%unfiltered = temps(:,1);
+%outfile = [outpDir "unfiltered.csv"];
+%csvwrite(outfile, unfiltered);
+%
+%% read FIR filtered output from c program
+%infile = [inpDir "filtered.csv"];
+%filtered = csvread(infile);
+%
+%figure(2); hold on;
+%plot(time, unfiltered);
+%plot(time, filtered);
+%title("FIR filter response (C implementation)");
+%legend("unfiltered signal","firC",'location','southeast');
+%grid on;
+%hold off;
+%
